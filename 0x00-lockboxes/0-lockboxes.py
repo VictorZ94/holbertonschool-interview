e3aa6478b0ba30d6afaa27c39b68b71912498d46
Victor Zuluaga <zrvictor@outlook.com>
2021-04-20 19:17:37 -0500

    Challenge resolved -  lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    length_boxes = len(boxes)
+    check_list = list(map(lambda num: num + 1, range(length_boxes)))
+    i = 0
+    while i < length_boxes:
+        j = 0
+        while j < len(boxes[i]):
+            if boxes[i][j] in check_list:
+                break
+            j += 1
+        if len(boxes[i]) > 0 and j != len(boxes[i]):
+            check_list.remove(boxes[i][j])
+        i += 1
+
+    if len(check_list) == 1:
+        return True
+    else:
+        return False
