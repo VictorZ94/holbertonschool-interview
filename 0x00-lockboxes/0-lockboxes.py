deab1d637dbcdf61f3df5d3da5b0dd503696728a
Victor Zuluaga <zrvictor@outlook.com>
2021-04-21 20:05:28 -0500

    lockboxes: find whether a boxes can open or not

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""lockboxes: find whether a boxes can open or not.
+"""
+
+
 def canUnlockAll(boxes):
+    """lockboxes: find whether a boxes can open or not.
+    """
     length_boxes = len(boxes)
     check_list = list(map(lambda num: num + 1, range(length_boxes)))
     i = 0
