685497c71e534aba8a675da83933d12cb9b68e7d
Victor Zuluaga <zrvictor@outlook.com>
2021-04-28 09:14:44 -0500

    it was lacking: when new node insert at the end of the linked list this last does not was pointig to NULL

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -44,8 +44,10 @@ listint_t *insert_node(listint_t **head, int number)
 			current = current->next;
 		}
 		if (check == 0)
+		{
+			newnode->next = NULL;
 			current->next = newnode;
+		}
 	}
 	return (newnode);
 }
-
