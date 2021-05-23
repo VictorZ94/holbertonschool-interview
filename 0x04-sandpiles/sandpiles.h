21eadec5b585cbcd77e3997a9075871a006da737
Victor Zuluaga <zrvictor@outlook.com>
2021-05-22 21:42:55 -0500

    Header for sandpile algorithm

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,11 @@
+#ifndef SANDPILE_H
+#define SANDPILE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void sum_grids(int grid1[3][3], int grid2[3][3]);
+void print_mygrid(int grid[3][3]);
+void toppled(int grid1[3][3]);
+
+#endif /* Sandpile  */
