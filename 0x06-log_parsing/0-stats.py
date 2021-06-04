a941e6d1e82750dfa364ebf43390d57bae96f7af
Victor Zuluaga <zrvictor@outlook.com>
2021-06-04 14:59:32 -0500

    stats function

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" pasing metrics of a file or server"""
+"""pasing metrics of a file or server"""
 from sys import stdin
 from collections import Counter, OrderedDict
 
 
 def print_metrics(list_metrics, file_size):
-    """ print metrics size file and amount of status code"""
+    """print metrics size file and amount of status code"""
     count_scode = Counter(list_metrics)
     print(f"File size: {file_size}")
     sorted_items = OrderedDict(sorted(count_scode.items()))
@@ -13,8 +13,11 @@ def print_metrics(list_metrics, file_size):
         print(f"{key}: {value}")
 
 
+"""pasing metrics of a file or server."""
+
+
 if __name__ == '__main__':
-    """ pasing metrics of a file or server"""
+    """pasing metrics of a file or server."""
     try:
         check = 1
         fsize = 0
