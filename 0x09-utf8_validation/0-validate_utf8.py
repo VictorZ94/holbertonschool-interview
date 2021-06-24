2beda5b610db47ac19664af46b5101d24d04917b
Victor Zuluaga <zrvictor@outlook.com>
2021-06-23 19:01:41 -0500

    UTF-8 Algorithm

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Test UTF-8
+"""
+def validUTF8(data):
+    """
+    Test UTF-8
+    """
+    for i in data:
+        if i >= 256:
+            return False
+    return True
