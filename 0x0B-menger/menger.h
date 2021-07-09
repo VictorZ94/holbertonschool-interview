3563e4f16091902b6a6db0437491d9ec22b093eb
Victor Zuluaga <zrvictor@outlook.com>
2021-07-09 11:10:31 -0500

    Menger project

---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,10 @@
+#ifndef MENGER_H
+#define MENGER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void menger(int level);
+int return_(int level);
+int filled(int col, int row);
+
+#endif /* Menger */
