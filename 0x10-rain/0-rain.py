c2df2b8ddec2ed3dd3a08dc440bba1c60593285f
Victor Zuluaga <zrvictor@outlook.com>
2021-08-17 20:41:33 -0500

    rain problem

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" calculate amount water retained between two walls
+"""
+
+
+def rain(walls):
+    """ rain calculates
+
+    Args:
+        walls (list): non-negative integers representing
+        the heights of walls with unit width 1
+
+    Returns:
+        [int]: represent the sum of all water retained
+    """
+    if walls is None or walls == []:
+        return 0
+
+    list1=[]
+    list2=[]
+    for i, v in enumerate(walls, 1):
+        if v != 0:
+            list1.append(i)
+            list2.append(v)
+
+    add=0
+    for j in range(len(list1)):
+        i1 = list1[j]
+        if j < len(list1) - 1:
+            i2 = list1[j+1]
+        index = i2 - i1 - 1
+        v1 = list2[j]
+        if j < len(list1) - 1:
+            v2 = list2[j + 1]
+        value = v1 if v1 < v2 else v2
+        add += index * value
+        if j+1 == len(list1) - 1:
+            return add
