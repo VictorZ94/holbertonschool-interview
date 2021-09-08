95ebd0aebc72be5fe9e13324afd508cab04fe15e
Victor Zuluaga <zrvictor@outlook.com>
2021-09-08 17:05:32 -0500

    Query API REDDIT

---
+++ 0x13-count_it/bash_script.sh
@@ -0,0 +1,4 @@
+python3 0-main.py programming 'react python java javascript scala no_results_for_this_one'
+python3 0-main.py programming 'JavA java'
+python3 0-main.py not_a_valid_subreddit 'python java javascript scala no_results_for_this_one'
+python3 0-main.py not_a_valid_subreddit 'python java'
