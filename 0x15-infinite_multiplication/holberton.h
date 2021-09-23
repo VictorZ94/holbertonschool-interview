99680ec970681ef4695d024277a46dec062370f3
Victor Zuluaga <zrvictor@outlook.com>
2021-09-22 19:57:53 -0500

    prototypes

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,15 @@
+#ifndef MUL_H
+#define MUL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int _putchar(char c);
+void Error_message(void);
+int _isdigit(char c);
+int _strlen(char *s);
+int *multiply(char *num1, char *num2);
+int *vector(int len);
+void reverse_array(int *a, int n);
+
+#endif /* Big Multiplication */
